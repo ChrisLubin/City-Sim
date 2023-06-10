ab1b9a547ab895868a7830bf1ff4e4a26fe9b752
ChrisLubin <chrisx700@gmail.com>
2023-06-09 23:50:07 -0400

    Import system and utility scripts

---
+++ Assets/Scripts/Systems/RpcSystem.cs
@@ -0,0 +1,7 @@
+using System;
+using System.Linq;
+using Unity.Netcode;
+
+public class RpcSystem : NetworkedStaticInstanceWithLogger<RpcSystem>
+{
+}
---
+++ Assets/Scripts/Utilities/Helpers.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+/// <summary>
+/// A static class for general helpful methods
+/// </summary>
+public static class Helpers
+{
+    /// <summary>
+    /// Destroy all child objects of this transform (Unintentionally evil sounding).
+    /// Use it like so:
+    /// <code>
+    /// transform.DestroyChildren();
+    /// </code>
+    /// </summary>
+    public static void DestroyChildren(this Transform t)
+    {
+        foreach (Transform child in t) Object.Destroy(child.gameObject);
+    }
+
+    public static T[] ToArray<T>(IReadOnlyList<T> readOnlyList)
+    {
+        List<T> list = new();
+
+        foreach (T element in readOnlyList)
+        {
+            list.Add(element);
+        }
+
+        return list.ToArray<T>();
+    }
+}
