478b2dab1204f10709c869e6f872eb4520c15a76
ChrisLubin <chrisx700@gmail.com>
2023-07-25 21:09:57 -0400

    Completed setup work for pathfinding system to respect lane laws

--- Assets/Scripts/Vehicle/VehicleAiPathController.cs
+++ Assets/Scripts/Vehicle/VehicleAiPathController.cs
@@ -9,7 +9,7 @@ public class VehicleAiPathController : NetworkBehaviour
     private VehicleSeatController _seatController;
 
     private Seeker _seeker;
-    private Path _currentPath = null;
+    private Path _fullRoute = null;
     private int _currentWaypoint = 0;
     [SerializeField] private float _waypointEnteredDistance = 3;
 
@@ -17,7 +17,13 @@ public class VehicleAiPathController : NetworkBehaviour
 
     public event Action<Vector3> OnNextNodeChange;
 
-    private readonly string[] _GRAPH_NAMES = new string[] { "Graph1", "Graph2" };
+    private const string _ALL_DIRECTIONS_GRAPH_NAME = "AllDirections";
+    private const string _NORTH_DIRECTION_GRAPH_NAME = "North";
+    private const string _EAST_DIRECTION_GRAPH_NAME = "East";
+    private const string _SOUTH_DIRECTION_GRAPH_NAME = "South";
+    private const string _WEST_DIRECTION_GRAPH_NAME = "West";
+
+    private Vector3 _test;
 
     private void Awake()
     {
@@ -33,22 +39,22 @@ public class VehicleAiPathController : NetworkBehaviour
 
     private void Start()
     {
-        this.RefreshPath();
+        this.GetWholeRoute();
     }
 
     private void Update()
     {
-        if (!this.IsOwner || !this._seatController.HasAiInDriverSeat || this._target == null || this._currentPath == null) { return; }
+        if (!this.IsOwner || !this._seatController.HasAiInDriverSeat || this._target == null || this._fullRoute == null) { return; }
 
         float distanceToNextWaypoint;
 
         // Check in a loop if we are close enough to the current waypoint to switch to the next one
         while (true)
         {
-            distanceToNextWaypoint = Vector3.Distance(transform.position, this._currentPath.vectorPath[this._currentWaypoint]);
+            distanceToNextWaypoint = Vector3.Distance(transform.position, this._fullRoute.vectorPath[this._currentWaypoint]);
 
             if (distanceToNextWaypoint >= this._waypointEnteredDistance) { break; }
-            if (this._currentWaypoint + 1 >= this._currentPath.vectorPath.Count)
+            if (this._currentWaypoint + 1 >= this._fullRoute.vectorPath.Count)
             {
                 // We reached the last waypoint
                 break;
@@ -56,36 +62,20 @@ public class VehicleAiPathController : NetworkBehaviour
 
             // Entered waypoint
             this._currentWaypoint++;
-            OnNextNodeChange?.Invoke(this._currentPath.vectorPath[this._currentWaypoint]);
+            OnNextNodeChange?.Invoke(this._fullRoute.vectorPath[this._currentWaypoint]);
         }
     }
 
-    private async void RefreshPath()
+    private async void GetWholeRoute()
     {
         this._currentWaypoint = 0;
-        this._currentPath = null;
+        this._fullRoute = null;
 
-        string correctGraph = null;
-        float shortestStartingDistance = float.MaxValue;
-
-        foreach (string graphName in _GRAPH_NAMES)
-        {
-            Path path = this._seeker.StartPath(transform.position, this._target.position, (Path p) => { }, GraphMask.FromGraphName(graphName));
-            await path.WaitForPath();
-            float startingDistance = Vector3.Distance(transform.position, path.vectorPath[0]);
-
-            if (correctGraph == null || startingDistance < shortestStartingDistance)
-            {
-                correctGraph = graphName;
-                shortestStartingDistance = startingDistance;
-            }
-        }
-
-        Path correctPath = this._seeker.StartPath(transform.position, this._target.position, (Path p) => { }, GraphMask.FromGraphName(correctGraph));
-        await correctPath.WaitForPath();
-        this._currentPath = correctPath;
+        Path route = this._seeker.StartPath(transform.position, this._target.position, (Path p) => { }, GraphMask.FromGraphName(_ALL_DIRECTIONS_GRAPH_NAME));
+        await route.WaitForPath();
+        this._fullRoute = route;
         this._seeker.drawGizmos = true;
         this._seeker.detailedGizmos = true;
-        OnNextNodeChange?.Invoke(this._currentPath.vectorPath[this._currentWaypoint]);
+        OnNextNodeChange?.Invoke(this._fullRoute.vectorPath[this._currentWaypoint]);
     }
 }
